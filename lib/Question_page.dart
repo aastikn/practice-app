dbe6bc5d1e075d8ecbabbc889e8e4a9410937904
Aastik Narang <aastiksimple@gmail.com>
2023-08-16 20:23:17 +0530

    added questions page

---
+++ lib/Question_page.dart
@@ -0,0 +1,169 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a blue toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Colors.amber,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+          Align(
+          alignment: Alignment.topCenter,
+          child:   ClipPath(
+            clipper: CustomShape(), // this is my own class which extendsCustomClipper
+            child: Container(
+                height: 150,
+                color: Colors.amber),
+          ),
+        ),
+        SizedBox(
+          height: 100,
+        ),
+        Align(
+          alignment: Alignment.center,
+          child: Container(
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: Colors.amber,),
+
+              height: 100,
+              width: 200,
+              child: Align(
+                alignment: Alignment.center,
+                child: Text( style: TextStyle(fontStyle: FontStyle.italic,fontSize: 30.1 ),
+                    "Question 1"
+                ),
+              )
+          ),
+        ),
+        SizedBox(
+          height: 100,
+        ),
+        Container(
+            height: 100,
+            width: 300,
+            padding: EdgeInsets.all(20),
+            child: TextField(
+              decoration: InputDecoration(
+                  enabledBorder:OutlineInputBorder(
+                      borderSide:
+                      BorderSide(
+                          width: 4.0,
+                          color: Colors.amber,
+                          style: BorderStyle.solid
+                      )
+
+                  ),
+
+                  border: OutlineInputBorder(borderRadius:BorderRadius.circular(20),)),
+      ),
+    )
+
+    ],
+    ),
+    ),
+    );
+  }
+}
+class CustomShape extends CustomClipper<Path> {
+  @override
+  getClip(Size size) {
+    double height = size.height;
+    double width = size.width;
+    var path = Path();
+    path.lineTo(0, height - 150);
+    path.quadraticBezierTo(width / 2, height, width, height);
+    path.lineTo(width, 0);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper oldClipper) {
+    return true;
+  }
+}
+
