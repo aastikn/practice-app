e5efe753c64b8706a1ebda706bcf8e2a6404c1d8
aastikn <aastiksimple@gmail.com>
2023-08-24 19:36:06 +0530

    feat:qrscanner

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ch2/main.dart';
+import 'package:ch2/Question_page.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
